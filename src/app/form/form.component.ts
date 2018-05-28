95828d1bb6d238b70e2dfda7cc77d0b1ec87141d
Blankphrase <ckasera6486@gmail.com>
2018-05-28 22:54:29 +0300

    creation of class FormComponent to handle submitted quotes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  quotes = new Quote("", "", "", 0, 0, new Date());
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
+  
   constructor() { }
 
   ngOnInit() {
