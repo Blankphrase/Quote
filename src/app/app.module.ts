90cddf0961fe966c6779ffe5329cb3e4489fc3cb
Blankphrase <ckasera6486@gmail.com>
2018-05-28 21:58:28 +0300

    creation of form,quote and form-details components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
+import { FormDetailsComponent } from './form-details/form-details.component';
+import { DateCountPipe } from './date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    FormComponent,
+    FormDetailsComponent,
+    DateCountPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
